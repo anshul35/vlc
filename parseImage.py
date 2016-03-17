c7f3a85697f96439918dca373f72cd5a70627715
Abhishek Ambastha <aambasth@cisco.com>
2016-03-16 23:31:05 -0700

    Happy with parsing

--- parseImage.py
+++ parseImage.py
@@ -2,14 +2,15 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-THRESHOLD = 150
+
 def parse(filename):
-    im_gray = cv2.imread(filename)
-    im_gray = cv2.cvtColor(im_gray, cv2.COLOR_BGR2GRAY)
-    signal_arr = np.sum(im_gray, 0)
-    cv2.imshow('orig', im_gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    im_gray = cv2.imread(filename, 0)
+    #im_gray = cv2.GaussianBlur(im_gray, (5,5), 0)
+    img = cv2.adaptiveThreshold(im_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 2)
+    signal_arr = np.sum(img, 0)
+
+    cv2.imshow('orig', img)
+    THRESHOLD = np.mean(signal_arr)
     signal_arr_channel = signal_arr
     low_values_indices = signal_arr_channel < THRESHOLD 
     high_values_indices = signal_arr_channel >= THRESHOLD
@@ -19,6 +20,8 @@ def parse(filename):
 
     plt.plot(signal_arr)
     plt.show()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     return signal_arr_channel
 
 if __name__ == "__main__":
