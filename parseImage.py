fea447e580c5064ab88cfb12c41ea13bcae070f2
anshul agrawal <anshuagr@cisco.com>
2016-03-16 23:02:30 -0700

    Changes in parsing

--- parseImage.py
+++ parseImage.py
@@ -2,22 +2,25 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+THRESHOLD = 150
 def parse(filename):
     im_gray = cv2.imread(filename)
     im_gray = cv2.cvtColor(im_gray, cv2.COLOR_BGR2GRAY)
     signal_arr = np.sum(im_gray, 0)
+    cv2.imshow('orig', im_gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     signal_arr_channel = signal_arr
-    low_values_indices = signal_arr_channel < 100
-    high_values_indices = signal_arr_channel >= 100
+    low_values_indices = signal_arr_channel < THRESHOLD 
+    high_values_indices = signal_arr_channel >= THRESHOLD
 
     signal_arr_channel[low_values_indices] = 0
     signal_arr_channel[high_values_indices] = 1
-#    import pdb; pdb.set_trace()
 
-    plt.plot(signal_arr[0])
+    plt.plot(signal_arr)
     plt.show()
     return signal_arr_channel
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     parse("test.jpg")
     
